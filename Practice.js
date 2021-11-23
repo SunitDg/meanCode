49e3f9fb05f71e76fb9709f614e61afa581699ad
SunitDg <dasguptasunit@gmail.com>
2021-11-23 22:32:17 +0530

    Initial Commit

---
+++ Practice.js
@@ -0,0 +1,10 @@
+<srcipt>
+    const arr = ['js','Angular','React','Python','Node'];
+
+    let arr1 = arr[0];
+    let arr2 = arr[1];
+    let arr3 = arr[2];
+
+    let [arr1,arr2,arr3,arr4,arr5] = arr;
+    console.log(arr2);  {/* /* prints Angular */}
+</srcipt>
